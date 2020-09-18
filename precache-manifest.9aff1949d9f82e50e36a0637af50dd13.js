1bcc86a7460794267f38059ebb96a7667a98d130
Sarad Subedi <subedisarad27@gmail.com>
2020-09-18 13:38:45 +0545

    Updates

---
+++ precache-manifest.9aff1949d9f82e50e36a0637af50dd13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990abc8dab531a7e984aee53c285a8fa",
+    "url": "/React-Calc/index.html"
+  },
+  {
+    "revision": "432fccc4052281d265e1",
+    "url": "/React-Calc/static/css/main.04b63b5f.chunk.css"
+  },
+  {
+    "revision": "54e071a3686accfc3861",
+    "url": "/React-Calc/static/js/2.70003e79.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/React-Calc/static/js/2.70003e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432fccc4052281d265e1",
+    "url": "/React-Calc/static/js/main.ce9f1960.chunk.js"
+  },
+  {
+    "revision": "c1b6de14790e0dc6821f",
+    "url": "/React-Calc/static/js/runtime-main.bf28ded2.js"
+  }
+]);
